7c9b02702c88e4b4fb914765b969d615bcaff9cd
drjpoudel <drjpoudel@gmail.com>
2024-08-06 23:09:27 +0545

    Update nepalidate.js

--- nepalidate.js
+++ nepalidate.js
@@ -1,4 +1,4 @@
-  const nepaliYearsAndDaysInMonths = [
+     const nepaliYearsAndDaysInMonths = [
             [2000, 30, 32, 31, 32, 31, 30, 30, 30, 29, 30, 29, 31],
             [2001, 31, 31, 32, 31, 31, 31, 30, 29, 30, 29, 30, 30],
             [2081, 30, 32, 31, 32, 31, 30, 30, 30, 29, 30, 30, 30]
@@ -66,4 +66,3 @@
         }
 
         document.addEventListener('DOMContentLoaded', displayNepaliDate);
-    

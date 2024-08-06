d4f62ae3836cd52555ad7aa1ac17d48d30f4541a
drjpoudel <drjpoudel@gmail.com>
2024-08-06 20:59:28 +0545

    Update nepalidate.js

--- nepalidate.js
+++ nepalidate.js
@@ -1,23 +1,29 @@
+<script>
         const nepaliYearsAndDaysInMonths = [
             [2000, 30, 32, 31, 32, 31, 30, 30, 30, 29, 30, 29, 31],
             [2001, 31, 31, 32, 31, 31, 31, 30, 29, 30, 29, 30, 30],
-            [2081, 30, 32, 31, 32, 31, 30, 30, 30, 29, 30, 30, 30]
+            [2082, 30, 32, 31, 32, 31, 30, 30, 30, 29, 30, 30, 30]
         ];
 
         const monthNames = ["बैशाख", "जेष्ठ", "आषाढ़", "श्रावण", "भाद्र", "आश्विन", "कार्तिक", "मंसिर", "पुष", "माघ", "फाल्गुण", "चैत्र"];
         const weekdayNames = ["आइतबार", "सोमबार", "मङ्गलबार", "बुधबार", "बिहिबार", "शुक्रबार", "शनिबार"];
 
+        function arabicToDevanagariNumber(num) {
+            const devanagariDigits = ['०', '१', '२', '३', '४', '५', '६', '७', '८', '९'];
+            return num.toString().split('').map(digit => devanagariDigits[digit]).join('');
+        }
+
         function getNepaliDate() {
             const today = new Date();
-            const nepaliYear = 2082;
+            const nepaliYear = 2082; // For example, update this as needed
             const nepaliMonth = 4;  // For example, श्रावण
             const nepaliDay = today.getDate();
             const weekdayIndex = today.getDay();
 
             return {
-                year: nepaliYear,
+                year: arabicToDevanagariNumber(nepaliYear),
                 month: monthNames[nepaliMonth],
-                day: nepaliDay,
+                day: arabicToDevanagariNumber(nepaliDay),
                 weekday: weekdayNames[weekdayIndex]
             };
         }

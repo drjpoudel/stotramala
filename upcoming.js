c37899f7587607a19ed0ec984186cd802b7f5940
drjpoudel <drjpoudel@gmail.com>
2024-11-22 23:48:51 +0545

    Update upcoming.js

--- upcoming.js
+++ upcoming.js
@@ -1,7 +1,29 @@
  const events = [
             { date: new Date('2024-10-17'), text: " शरद पूर्णिमा " },
             { date: new Date('2024-11-30'), text: " बाला चतुर्दशी " },
-            { date: new Date('2024-12-06'), text: " सीता राम विवाह उत्सव " }
+            { date: new Date('2024-12-06'), text: " सीता राम विवाह उत्सव " },
+            { date: new Date('2024-12-06'), text: " " },
+            { date: new Date('2024-11-06'), text: " " },
+            { date: new Date('2024-11-06'), text: " " },
+            { date: new Date('2024-11-06'), text: " " },
+            { date: new Date('2024-11-06'), text: " " },
+            { date: new Date('2024-11-06'), text: " " },
+            { date: new Date('2024-11-06'), text: " " },
+            { date: new Date('2024-11-06'), text: " " },
+            { date: new Date('2024-11-06'), text: " " },
+            { date: new Date('2024-11-06'), text: " " },
+            { date: new Date('2024-11-06'), text: " " },
+            { date: new Date('2024-11-06'), text: " " },
+            { date: new Date('2024-11-06'), text: " " },
+            { date: new Date('2024-11-06'), text: " " },
+            { date: new Date('2024-11-06'), text: " " },
+            { date: new Date('2024-11-06'), text: " " },
+            { date: new Date('2024-11-06'), text: " " },
+            { date: new Date('2024-11-06'), text: " " },
+            { date: new Date('2024-11-06'), text: " " },
+            { date: new Date('2024-11-06'), text: " " },
+            { date: new Date('2024-11-06'), text: " " }
+
              
   ];
 
